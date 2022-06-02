0bce4e0a0c306aa7bf526d8f91a4a4ea7a04e070
Alexandre Costa <alexandre97costa@gmail.com>
2022-06-02 13:29:14 +0100

    Update formularios_v2.js

--- frontend/src/view/backoffice/formularios_v2.js
+++ frontend/src/view/backoffice/formularios_v2.js
@@ -21,25 +21,25 @@ export default function FormulariosComponente() {
     function LoadForms() {
         return forms.map(form => {
             return (
-                <div class="col-12 d-flex flex-column h-sm-100">
+                <div className="col-12" key={form.id}>
 
-   
-
-
-                    <div class="accordion accordion-flush m-2" id={"accordion" + form.id}>
-                        <div class="accordion-item">
-                            <h2 class="accordion-header" id="flush-headingOne">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={"#a" + form.id} aria-expanded="false" aria-controls="flush-collapseOne">
-                                <div className="fs-4 fw.bold text-warning">
+                    <div className="fs-4 fw.bold text-warning">
                         {form.descricao}
                     </div>
+
+
+                    <div className="accordion accordion-flush m-2" id={"accordion-" + form.id} >
+                        <div className="accordion-item">
+                            <h2 className="accordion-header" id={"accordion-header-" + form.id}>
+                                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={"#a" + form.id} >
+                                    aqui fica o nome do grupo
                                 </button>
                             </h2>
-                            <div id={"a" + form.id} class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-                                <div class="accordion-body">
+                            <div id={"a" + form.id} className="accordion-collapse collapse" data-bs-parent={"#accordion-" + form.id}>
+                                <div className="accordion-body">
 
 
-                                    <table class="table">
+                                    <table className="table">
                                         <thead>
                                             <tr>
                                                 <td style={{ width: "20%" }}>Titulo da Pergunta</td>
@@ -53,38 +53,38 @@ export default function FormulariosComponente() {
                                             <tr>
 
                                                 <th scope="row">
-                                                    <input type="email" class="form-control" value=""></input>
+                                                    <input type="email" className="form-control" value=""></input>
                                                 </th>
 
                                                 <td>
-                                                    <input type="email" class="form-control" value=""></input>
+                                                    <input type="email" className="form-control" value=""></input>
                                                 </td>
 
                                                 <td>
-                                                    <div class="dropdown">
-                                                        <button class="btn outline-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" value="checkbox">
+                                                    <div className="dropdown">
+                                                        <button className="btn outline-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" value="checkbox">
 
                                                         </button>
-                                                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                                        <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
                                                             {/* Tenho que fazer aqui um get da inf da BD */}
 
-                                                            <li><a class="dropdown-item" href="#">Acti6on</a></li>
-                                                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                                                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                                            <li><a className="dropdown-item" href="#">Acti6on</a></li>
+                                                            <li><a className="dropdown-item" href="#">Another action</a></li>
+                                                            <li><a className="dropdown-item" href="#">Something else here</a></li>
 
                                                         </ul>
                                                     </div>
                                                 </td>
                                                 <td>
 
-                                                    <input type="number" class="form-control" value=""></input>
+                                                    <input type="number" className="form-control" value=""></input>
 
                                                 </td>
 
                                                 <td>
 
-                                                    <button type="button" class="btn-sm btn btn-success me-1"> <i class="text-white bi bi-save m-1"></i>Guardar</button>
-                                                    <button type="button" class="btn-sm btn btn-danger ms-1"> <i class="bi bi-folder-x"></i> Eliminar</button>
+                                                    <button type="button" className="btn-sm btn btn-success me-1"> <i className="text-white bi bi-save m-1"></i>Guardar</button>
+                                                    <button type="button" className="btn-sm btn btn-danger ms-1"> <i className="bi bi-folder-x"></i> Eliminar</button>
 
                                                 </td>
                                             </tr>
@@ -94,7 +94,7 @@ export default function FormulariosComponente() {
                             </div>
                         </div>
                     </div>
-                </div >
+                </div>
             )
         })
     }
@@ -108,28 +108,26 @@ export default function FormulariosComponente() {
 
                 <div className="col overflow-auto h-sm-100">
 
-                    <main className="row px-5">
+                    <div className="row px-4">
                         {/* Titulo */}
                         <div className='col-12 my-4'>
                             <div className='display-3 text-indigo'>
                                 Formulários
                             </div>
                         </div>
+                    </div>
 
-                        <div className='col-12'>
-                            <div className='row'>
-
-                                <LoadForms />
-                                {/*Aqui será onde ficará o conteúdo dos formulários!?*/}
+                    <div className='row px-4'>
 
 
+                        <LoadForms />
+                        {/*Aqui será onde ficará o conteúdo dos formulários!?*/}
 
 
 
-                            </div>
-                        </div>
 
-                    </main>
+
+                    </div>
 
                 </div>
 
