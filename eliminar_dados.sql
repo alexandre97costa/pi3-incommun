5325de1f54b2e9abd0e5ceafc1f29a3f8b5448ba
Alexandre Costa <alexandre97costa@gmail.com>
2022-06-03 12:49:45 +0100

    eliminar_dados.sql
    e mudança de nome do sql que insere dados

---
+++ eliminar_dados.sql
@@ -0,0 +1,13 @@
+truncate table 
+    cliente,
+    estado_pedido,
+    formulario,
+    grupo,
+    motivo_recusa_pedido,
+    pedido,
+    pergunta,
+    resposta,
+    user_incommun,
+    user_incommun_role
+    restart identity 
+    cascade;
