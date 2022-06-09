1f5b437cdae50bbe614b2f8e51f78e400bf7b40f
Guilherme Duarte <estgv16396@alunos.estgv.ipv.pt>
2022-06-09 09:46:57 +0100

    .

--- operacoes.sql
+++ operacoes.sql
@@ -9,47 +9,57 @@ select - 5
 
 /**************************LISTA DE OPERAÇÕES**************************/
 
-/*****INSERT*****/
-/*1.1 Inserir um Cliente*/
+/*1 Inserir um Cliente*/
 INSERT INTO cliente (id, nome, email, empresa, tlm, created_at, updated_at) VALUES
 
-/*1.2 Inserir um utilizador da incommun*/
+/*2 Inserir um utilizador da incommun*/
 INSERT INTO user_incommun_role (id, descricao, obs) VALUES
 
-/*1.3 Inserir novo pedido*/
+/*3 Inserir novo pedido*/
 INSERT INTO pedido (id, cliente_id, valor_total, estado_id, motivo_id, created_at, updated_at) VALUES
 
-/*****SELECT*****/
-/*2.1 Consulta que devolve o numero total de pedidos (orçamentos) */
+/*4 Consulta que devolve o numero total de pedidos (orçamentos) */
 select SUM (id) as "Total de Orçamentos" from pedido
 
-/*2.2 Consulta que devolve o numero total de pedidos (orçamentos) pendentes */
+/*5 Consulta que devolve o numero total de pedidos (orçamentos) pendentes */
 select SUM (id) as "Orçamentos Pendentes" from pedido
 where estado_id = 1
 
-/*2.3 Consulta que devolve o numero total de pedidos (orçamentos) aceites */
+/*6 Consulta que devolve o numero total de pedidos (orçamentos) aceites */
 select SUM (id) as "Orçamentos Aceites" from pedido
 where estado_id = 3
 
-/*2.4 Consulta que devolve o numero total de pedidos (orçamentos) recusados */
+/*7 Consulta que devolve o numero total de pedidos (orçamentos) recusados */
 select SUM (id) as "Orçamentos Recusados" from pedido
 where estado_id = 4
 
-/*2.5 Consulta que devolve o numero total de pedidos (orçamentos) enviados */
+/*8 Consulta que devolve o numero total de pedidos (orçamentos) enviados */
 select SUM (id) as "Orçamentos Enviados" from pedido
 where estado_id = 2
 
-/*****UPDATE*****/
-/*3.1 Atualizar valor unitário de pergunta*/
+/*9 Atualizar o valor unitario para todas as perguntas do grupo 5 */
 SELECT * FROM pergunta 
+UPDATE valor_unitario
+SET valor_unitario where grupo_id = 5;
 
-UPDATE pergunta
-SET valor_unitario;
+/*10 Eliminar pedidos que o estado seja "Recusado" */
+SELECT * FROM pedido;
+DELETE FROM pedido where estado_id = 4;
 
-/*3.2 */
+/*11 Consulta que devolve o pedido associado ao cliente com ID 10 com estado Aceite*/
+select * from pedido
+where cliente_id = 10 and estado_id = 3;
+
+/*12 Consulta que devolve os pedidos com valor ente 500 a 1000€ e foram recusados*/
+select * from pedido
+where valor_total between '500' and '1000' and estado_id = 4;
+
+/*13 Consulta que devolva o numero total de pedidos recusados com o seu motivo e valor total*/
+select SUM id as "Numero Pedidos", motivo_id as "Motivo", valor_total as "Valor Total", estado_id 
+from pedido
+where estado_id = 4;
 
 
-/*****DELETE*****/
-/*4.1 */
+
 
 /************************************************************* FIM OPERAÇÔES SQL *************************************************************/
