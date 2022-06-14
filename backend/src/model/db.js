edcaa8dbef85cebec82ab54eb3206208c739c9fe
Alexandre Costa <alexandre97costa@gmail.com>
2022-06-14 18:30:16 +0100

    heroku setup test

--- backend/src/model/db.js
+++ backend/src/model/db.js
@@ -1,13 +1,16 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize =  new Sequelize(
-    'pi3-sequelize',
-    'postgres',
-    'postgres',
+const sequelize = new Sequelize(
+    'd74bfknmfhh7mj',
+    'qlxojjauwpguba',
+    '3aa23e8c63b4a07eb64b73e4b3b6daface9691c725e92e2a8191ddf289a94bea',
     {
-        host: 'localhost',
+        host: 'ec2-34-247-72-29.eu-west-1.compute.amazonaws.com',
         port: '5432',
         dialect: 'postgres',
+        dialectOptions: {
+            ssl: { rejectUnauthorized: false }
+        },
         logging: false,
         define: {
             // hooks globais, atingem todos os modelos
@@ -28,4 +31,4 @@ const sequelize =  new Sequelize(
 
 sequelize.sync()
 
-module.exports =  sequelize 
+module.exports = sequelize 
