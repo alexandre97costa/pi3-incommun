d2468930eeb95e797da5e92e43643bc9eacef9a1
Dioo6 <105822207+Dioo6@users.noreply.github.com>
2022-07-03 10:47:41 +0100

    fix margins

--- frontend/src/view/forms/footer.js
+++ frontend/src/view/forms/footer.js
@@ -9,13 +9,13 @@ export default function FooterComponent() {
                         <h3 class="text-uppercase text-white">Estamos nas Redes!</h3>
                         <h6 className='text-white'>Visita-nos!</h6>
 
-                        <div className='text-purple margin-bottom-10 text-white'>
-                            <i class="bi bi-twitter margin-10 text-white"/>
-                            <i class="bi bi-instagram margin-10 text-white" />
-                            <i class="bi bi-facebook margin-10 text-white" />
-                            <i class="bi bi-linkedin margin-10 text-white" />
-                            <i class="bi bi-youtube margin-10 text-white" />
-                            <i class="bi bi-tiktok margin-10 text-white" />
+                        <div className='text-purple text-white'>
+                            <i class="bi bi-twitter m-4 text-white"/>
+                            <i class="bi bi-instagram m-4 text-white" />
+                            <i class="bi bi-facebook m-4 text-white" />
+                            <i class="bi bi-linkedin m-4 text-white" />
+                            <i class="bi bi-youtube m-4 text-white" />
+                            <i class="bi bi-tiktok m-4 text-white" />
                         </div>
                     </div>
                     <div class="col-lg-6 col-md-12 mb-4 mb-md-0 text-a-right">
