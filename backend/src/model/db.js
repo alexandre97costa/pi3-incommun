b9fbb60d65142f3d97893dd1f17ed3bb2d7bfc58
Dioo6 <105822207+Dioo6@users.noreply.github.com>
2022-07-11 22:45:38 +0100

    Changes in the frontend
    apesar dos botoes estarem la eles ainda nao funcionam como deviam

--- backend/src/model/db.js
+++ backend/src/model/db.js
@@ -4,7 +4,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(
     'pi3-sequelize',
     'postgres',
-    'pedrinho12',
+    'gatinhos',
     {
         host: 'localhost',
         port: '5432',
