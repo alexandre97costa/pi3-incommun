3707164c7fc0d7ebd0db3848b9a7368fa20dd9de
DaKxe <105804029+DaKxe@users.noreply.github.com>
2022-07-18 04:28:52 +0100

    Dropdown Update  💤💤💤
    Dropdown já aparece com a informação do tipo de pergunta, mas parece ter coisas a mais, e ainda nao é selecionavel
    (falta de input??) 💤💤💤

--- frontend/src/view/backoffice/Formulario/EditableRow.js
+++ frontend/src/view/backoffice/Formulario/EditableRow.js
@@ -37,11 +37,11 @@ export default function EditableRow({ handleCancelClick, id, pergunta, getForms 
     };
 
     useEffect(() => {
-        getTiposPergunta()
+        GetTiposPergunta()
     }, []);
 
 
-    function getTiposPergunta() {
+    function GetTiposPergunta() {
         axios.get(ip + '/forms/all_tipos_pergunta', authHeader())
             .then(res => {
                 console.table(res.data.TipoPergunta, ['id', 'nome'])
@@ -58,89 +58,130 @@ export default function EditableRow({ handleCancelClick, id, pergunta, getForms 
 
                 <tbody>
 
-                           
-                                <tr>
 
-                                    <td style={{ width: "30%" }}>
-                                        <input
-                                            className="form-control focus-warning"
-                                            type="text"
-                                            name="titulo"
-                                            required="required"
-                                            placeholder={pergunta.titulo}
-                                            value={edittitulopergunta}
-                                            onChange={e => seteditTituloPergunta(e.target.value)}
-                                        />
-                                    </td>
+                    <tr>
 
-                                    <td style={{ width: "40%" }}>
-                                        <input
-                                            className="form-control focus-warning"
-                                            type="text"
-                                            name="descricao"
-                                            required="required"
-                                            placeholder= {pergunta.descricao}
-                                            value={editdescricaopergunta}
-                                            onChange={e => seteditdescricaopergunta(e.target.value)}
-                                        />
-                                    </td>
+                        <td style={{ width: "30%" }}>
+                            <input
+                                className="form-control focus-warning"
+                                type="text"
+                                name="titulo"
+                                required="required"
+                                placeholder={pergunta.titulo}
+                                value={edittitulopergunta}
+                                onChange={e => seteditTituloPergunta(e.target.value)}
+                            />
+                        </td>
+
+                        <td style={{ width: "40%" }}>
+                            <input
+                                className="form-control focus-warning"
+                                type="text"
+                                name="descricao"
+                                required="required"
+                                placeholder={pergunta.descricao}
+                                value={editdescricaopergunta}
+                                onChange={e => seteditdescricaopergunta(e.target.value)}
+                            />
+                        </td>
 
 
 
-                                    <td style={{ width: "10%" }}>
+                        <td style={{ width: "10%" }}>
 
-                                        <div className="dropdown">
-                                            <button className="form-control focus-warning dropdown-toggle"
+
+
+
+
+
+                            <div className="dropdown">
+                                <button className="form-control focus-warning dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                    {pergunta.tipo_pergunta.titulo}
+                                </button>
+
+                                <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                    {listaTiposPergunta.map(TipoPergunta => {
+
+                                        return (
+
+                                            <li>
+                                                <a className="dropdown-item">
+                                                    {TipoPergunta.titulo}
+
+                                                </a>
+                                            </li>
+                                        )
+                                    })}
+
+                                </ul>
+                            </div>
+
+
+
+                        </td>
+
+
+
+                        {/* <div className="dropdown">
+                                            <input                                           
+                                            className="form-control focus-warning dropdown-toggle"                                         
                                                 type="button"
                                                 id="dropdownMenu2"
                                                 data-bs-toggle="dropdown"
-                                                aria-expanded="false">
-                                            </button>
+                                                aria-expanded="false"
+                                                required="required"
+                                                placeholder={pergunta.tipo_pergunta.titulo}
+                                                value= {edittipopergunta}
+                                                onChange={e => seteditvalorpergunta(e.target.value)}
+                                                >   
+                                               //////////    AQUI FICA "PRE DEFINIDO O TIPO DE PERGUNTA MAS NAO ATUALIZA A PERRGUNTA"   /////
+                                                {pergunta.tipo_pergunta.titulo}                                   
+                                                   
+                                            </input>
+
+                                            
                                             <ul className="dropdown-menu" aria-labelledby="dropdownMenu2">
                                                 <li>
                                                     <button className="dropdown-item"
                                                         type="button">
-                                                        {/* aqui clrho */}
+                                                        ///// aqui clrho///////
                                                     </button>
 
                                                 </li>
                                             </ul>
-                                        </div>
-
-                                    </td>
+                                        </div> */}
 
 
 
 
+                        <td style={{ width: "10%" }}>
+                            <input
+                                className="form-control focus-warning"
+                                type="number"
+                                name="valor_unitario"
+                                required="required"
+                                placeholder={pergunta.valor_unitario}
+                                value={editvalorpergunta}
+                                onChange={e => seteditvalorpergunta(e.target.value)}
+                            />
+                        </td>
 
-                                    <td style={{ width: "10%" }}>
-                                        <input
-                                            className="form-control focus-warning"
-                                            type="number"
-                                            name="valor_unitario"
-                                            required="required"
-                                            placeholder={pergunta.valor_unitario}
-                                            value={editvalorpergunta}
-                                            onChange={e => seteditvalorpergunta(e.target.value)}
-                                        />
-                                    </td>
 
+                        <td style={{ width: "10%" }}>
 
-                                    <td style={{ width: "10%" }}>
+                            <button type="submit"
+                                className="btn btn-success mx-2">
+                                <i className="bi bi-check-lg"></i>
+                            </button>
 
-                                        <button type="submit"
-                                            className="btn btn-success mx-2">
-                                            <i className="bi bi-check-lg"></i>
-                                        </button>
+                            <button type="button"
+                                className="btn btn-danger mx-2"
+                                onClick={handleCancelClick}>
+                                <i className="bi bi-x-lg"></i>
+                            </button>
 
-                                        <button type="button"
-                                            className="btn btn-danger mx-2"
-                                            onClick={handleCancelClick}>
-                                            <i className="bi bi-x-lg"></i>
-                                        </button>
-
-                                    </td>
-                                </tr>
+                        </td>
+                    </tr>
 
                 </tbody>
             </table>
